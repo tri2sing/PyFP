0c7b2d0346fe49284c9b1950bc05832d2e1789a4
Sameer Adhikari <tri2sing@yahoo.com>
2015-12-14 22:35:18 -0800

    Anscombe's Quartet using FP

--- features/anscombe.py
+++ features/anscombe.py
@@ -8,12 +8,16 @@ Created on Dec 14, 2015
 # https://en.wikipedia.org/wiki/Anscombe's_quartet
 
 import csv
+from collections import namedtuple
+from pprint import pprint
+
 
 def csv_row_iterator(source, delimiter='\t'):
     '''Return an iterator for the rows of a csv file '''
     return csv.reader(source, delimiter=delimiter)
 
-def csv_head_trimmer(row_iterator):
+
+def csv_row_trim_iterator(row_iterator):
     '''Remove the first three rows of anscombe's quartet data assuming a specific format'''
     title = next(row_iterator)
     assert(len(title) == 1 and title[0] == "Anscombe's Quartet")
@@ -22,12 +26,30 @@ def csv_head_trimmer(row_iterator):
     columns = next(row_iterator)
     assert(len(columns) == 8 and columns == ['x ', 'y ', 'x ', 'y ', 'x ', 'y ', 'x ', 'y'])
     return row_iterator
-    
+
+
+def series_point_generator(series_num, row_iterator):
+    ''' Each row of Anscombe's Quartet data consists four (x, y) points from different series.
+        This generator yields the points from all rows from a specified series (0, 1, 2, 3'''
+    Point = namedtuple('Point', ('x', 'y'))
+    for row in row_iterator:
+        yield Point(*row[2 * series_num : 2 * series_num + 2])
+             
 
 def main():
     with open('anscombe.txt') as source:
-        for row in csv_head_trimmer(csv_row_iterator(source)):
-            print(row)
+        # Materialize the iterator using tuple() so that it can be reused multiple times.
+        # In FP we prefer to use tuple compared to list because we prefer immutable objects.
+        data = tuple(csv_row_trim_iterator(csv_row_iterator(source)))
+        series_0 = tuple(series_point_generator(0, data))
+        series_1 = tuple(series_point_generator(1, data))
+        series_2 = tuple(series_point_generator(2, data))
+        series_3 = tuple(series_point_generator(3, data))
+        pprint(data)
+        pprint(series_0)
+        pprint(series_1)
+        pprint(series_2)
+        pprint(series_3)
 
 if __name__ == '__main__':
     main()
--- features/test_anscombe.py
+++ features/test_anscombe.py
@@ -6,17 +6,40 @@ Created on Dec 14, 2015
 
 import os
 from features.anscombe import csv_row_iterator,\
-    csv_head_trimmer
+    csv_row_trim_iterator, series_point_generator
 
 def test_csv_row_iterator():
     print(os.getcwd()) # If test is successful this does not get printed to stdout
+    # py.test looks for a file to open in the project folder rather than the 
+    # package folder unlike the case when we run the module itself, hence the 
+    # path to the file to open needs to be specified in this manner.
     with open(os.getcwd() + '/features/anscombe.txt') as source:
-        rows = list(csv_row_iterator(source, '\t'))
+        rows = tuple(csv_row_iterator(source, '\t'))
         assert(len(rows) == 14)
         
 
 def test_csv_head_trimmer():
     with open(os.getcwd() + '/features/anscombe.txt') as source:
-        rows = list(csv_head_trimmer(csv_row_iterator(source, '\t')))
+        rows = tuple(csv_row_trim_iterator(csv_row_iterator(source, '\t')))
         assert(len(rows) == 11)
     
+
+def test_series_point_generator():
+    with open(os.getcwd() + '/features/anscombe.txt') as source:
+        data = tuple(csv_row_trim_iterator(csv_row_iterator(source)))
+        
+        series_0 = tuple(series_point_generator(0, data))
+        assert(len(series_0) == 11)
+        assert(len(series_0[0]) == 2)
+        
+        series_1 = tuple(series_point_generator(1, data))
+        assert(len(series_1) == 11)
+        assert(len(series_1[0]) == 2)
+        
+        series_2 = tuple(series_point_generator(2, data))
+        assert(len(series_2) == 11)
+        assert(len(series_2[0]) == 2)
+        
+        series_3 = tuple(series_point_generator(3, data))
+        assert(len(series_3) == 11)
+        assert(len(series_3[0]) == 2)
