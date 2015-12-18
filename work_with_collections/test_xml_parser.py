fece7db4fa508863874571fcd6211a2a28eff1c4
Sameer Adhikari <tri2sing@yahoo.com>
2015-12-18 13:49:53 -0800

    completed parser

--- work_with_collections/test_xml_parser.py
+++ work_with_collections/test_xml_parser.py
@@ -5,9 +5,21 @@ Created on Dec 18, 2015
 '''
 
 import os
-from work_with_collections.xml_parser import xml_tree_to_row_iterator
+import urllib.request
+
+from work_with_collections.xml_parser import\
+     xml_tree_to_row_iterator,\
+     latitude_longitude_iterator
 
 def test_xml_tree_to_row_iterator():
-        with open(os.getcwd() + '/work_with_collections/locations.xml') as source:
-            rows = tuple(xml_tree_to_row_iterator(source))
-            assert(len(rows) == 6)
+    with open(os.getcwd() + '/work_with_collections/locations.xml') as source:
+        rows = tuple(xml_tree_to_row_iterator(source))
+        assert(len(rows) == 6)
+        assert(len(rows[0]) == 3)
+
+def test_latitude_longitude_iterator():
+    # Demonstrates use of an alternate function to open the file while reusing the iterators as-is.
+    with urllib.request.urlopen('file:' + os.getcwd() + '/work_with_collections/locations.xml') as source:
+        rows = tuple(latitude_longitude_iterator(xml_tree_to_row_iterator(source)))
+        assert(len(rows) == 6)
+        assert(len(rows[0]) == 2)
--- work_with_collections/xml_parser.py
+++ work_with_collections/xml_parser.py
@@ -5,6 +5,14 @@ Created on Dec 15, 2015
 '''
 
 import xml.etree.ElementTree as etr
+'''Demonstrate extraction of latitude and longitude values from a Keyhole Markup Language (KML) file using an almost 
+purely functional approach by explicitly separating low-level XML parsing from higher-level data reorganization. '''
+
+def split_at_comma(text):
+    '''Returns a specific split for a string.  
+    Maintain an syntactic uniformity with FP conventions, 
+    instead of directly using the string split function.'''
+    return text.split(',')
 
 def xml_tree_to_row_iterator(file_obj):
     '''Parses a file at low-level and yields an iterable sequence of tuples.
@@ -17,15 +25,30 @@ def xml_tree_to_row_iterator(file_obj):
     doc = etr.parse(file_obj)
     # A generator expression is a high-performance, memory-efficient generalization of list comprehension.
     # To create a generator expression, wrap a comprehension expression in () instead of [] or {}.
-    # The XM path in findall is case-sensitive, that is why the coordinates element.
-    return (coordinate.text.split(',') 
+    # The XMl path in findall is case-sensitive, that is why the "coordinates" element is lowercase.
+    # Instead of coordinate.text.split(',')  in the generator expression, we use our wrapper for uniform style.
+    return (split_at_comma(coordinate.text)
         for coordinate in doc.findall('./ns0:Document/ns0:Folder/ns0:Placemark/ns0:Point/ns0:coordinates', ns_map))
+
+
+def latitude_longitude_filter(longitude, latitude, altitude):
+    '''Filter out the altitude data and swap the order of longitude and latitude.
+    Maintains syntactic uniformity with FP conventions when used in caller. '''
+    return latitude, longitude
+
+
+def latitude_longitude_iterator(row_iterator):
+    '''Transform the low-level data to an higher-level application-specific data.'''
+    # Use tuple unpacking (*row) for the row to get the three values
+    return (latitude_longitude_filter(*row) for row in row_iterator)
+
         
 def main():
     with open('locations.xml') as source:
-        coordinates = xml_tree_to_row_iterator(source)
+        coordinates = latitude_longitude_iterator(xml_tree_to_row_iterator(source))
         for coordinate in coordinates:
             print(coordinate)
+
 
 if __name__ == '__main__':
     main()
