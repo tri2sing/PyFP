37b8e02545b5895a317ac6dc634d48e93b0baae7
Sameer Adhikari <tri2sing@yahoo.com>
2016-01-05 23:21:27 -0800

    updated with a technique

--- tuple_techniques/indexing.py
+++ tuple_techniques/indexing.py
@@ -14,6 +14,8 @@ point1 = (3, 8)
 point2 = (10, 5)
 segmt1 = (point1, point2)
 
+print('='*80)
+print('Approach 1')
 print('end point x coordinate using index notation point[0] = {}'.format(point2[0]))
 print('segment start point y coordinate using index notation segment[0][1] = {}'.format(segmt1[0][1]))
 
@@ -26,10 +28,24 @@ end = lambda segement: segement[1]
 xdim = lambda point: point[0]
 ydim = lambda point: point[1]
 
+print('='*80)
+print('Approach 2')
 print('end point x coordinate using prefix notation xdim(point) = {}'.format(xdim(point2)))
 print('segment start point y coordinate using prefix notation ydim(point(segment) = {}'.format(ydim(start(segmt1))))
 
 
+# An alternate lambda approach is to use the tuple unpacking to hide use of indexes.
+# As this is an example, primarily for demonstration, we will redefine the lambdas.
+
+start = lambda start, end: start
+end = lambda start, end: end
+xdim = lambda x, y: x
+ydim = lambda x, y: y
+
+print('='*80)
+print('Approach 3')
+print('end point x coordinate using tuple unpacking xdim(*point) = {}'.format(xdim(*point2)))
+print('segment start point y coordinate using tuple unpacking ydim(*point(*segment) = {}'.format(ydim(*start(*segmt1))))
 
 
-
+print('='*80)
