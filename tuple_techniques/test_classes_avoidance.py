b6e3a31510b58be353c0d73725c1188eabcb6dbd
Sameer Adhikari <tri2sing@yahoo.com>
2016-01-17 10:45:59 -0800

    added a test for statistical rank that show nested result

--- tuple_techniques/test_classes_avoidance.py
+++ tuple_techniques/test_classes_avoidance.py
@@ -19,7 +19,7 @@ def test_rank_nested_simple():
 
     data = expected
     returned = list(rank_nested_simple(data, key_function=lambda x:x[1][0]))
-    expected = [(0, (0, (2, 0.8))), (1, (1, (3, 1.2))), (2, (2, (5, 1.2))), (3, (3, (7, 2.3))),(4,  (4, (11, 18)))]
+    expected = [(0, (0, (2, 0.8))), (1, (1, (3, 1.2))), (2, (2, (5, 1.2))), (3, (3, (7, 2.3))), (4, (4, (11, 18)))]
     assert(returned == expected)
 
     
@@ -29,8 +29,12 @@ def test_rank():
     expected = [(1.0, 0.8), (2.5, 1.2), (2.5, 1.2), (4.0, 2.3), (5.0, 18)]
     assert(returned == expected)
     
-    data= ((2, 0.8), (3, 1.2), (5, 1.2), (7, 2.3), (11, 18))
+    data = ((2, 0.8), (3, 1.2), (5, 1.2), (7, 2.3), (11, 18))
     returned = list(rank(data, key_function=lambda x:x[1]))
     expected = [(1.0, (2, 0.8)), (2.5, (3, 1.2)), (2.5, (5, 1.2)), (4.0, (7, 2.3)), (5.0, (11, 18))]
     assert(returned == expected)
 
+    data = expected
+    returned = list(rank(data, key_function=lambda x:x[1][0]))
+    expected = [(1.0, (1.0, (2, 0.8))), (2.0, (2.5, (3, 1.2))), (3.0, (2.5, (5, 1.2))), (4.0, (4.0, (7, 2.3))), (5.0, (5.0, (11, 18)))]
+    assert(returned == expected)
