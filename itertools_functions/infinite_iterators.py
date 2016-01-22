bf8501f7c072fc9ed324060fa1c11fd043e796a8
Sameer Adhikari <tri2sing@yahoo.com>
2016-01-21 22:15:46 -0800

    Added usage of itertools.cycle()

--- itertools_functions/infinite_iterators.py
+++ itertools_functions/infinite_iterators.py
@@ -18,3 +18,11 @@ def custom_enumerate_zip_count(start, step, data_iterator):
     Unlike a regular enumerate() call where the the arithmetic series is [0, 1, 2,...].'''
     return zip(count(start, step), data_iterator)
 
+def divisible_by_n(limit, n):
+    ''' Returns a iterator for the subset of integers in [0, limit] divisible by n.
+    Please note that this is an example of the use of the cycle() function.
+    It is not the most efficient solution to the problem of divisible by n.'''
+    cycle_n = (i == 0 for i in cycle(range(n)))
+    matches = zip(range(limit + 1), cycle_n)
+    return (num for num, match in matches if match == True)
+    
--- itertools_functions/test_infinite_iterators.py
+++ itertools_functions/test_infinite_iterators.py
@@ -4,7 +4,7 @@ Created on Jan 20, 2016
 @author: Sameer Adhikari
 '''
 
-from itertools_functions.infinite_iterators import custom_enumerate, custom_enumerate_zip_count
+from itertools_functions.infinite_iterators import custom_enumerate, custom_enumerate_zip_count, divisible_by_n
 
 
 def test_custom_enumerate():
@@ -18,3 +18,12 @@ def test_custom_enumerate_zip_count():
     returned = list(custom_enumerate_zip_count(1, 3, iter(given)))
     expected = [(1, 1), (4, 2), (7, 3), (10, 4), (13, 5)]
     assert(returned == expected)
+
+def test_divisible_by_n():
+    returned = list(divisible_by_n(15, 3))
+    expected = [0, 3, 6, 9, 12, 15]
+    assert (returned == expected)
+    
+    returned = list(divisible_by_n(15, 5))
+    expected = [0, 5, 10, 15]
+    assert (returned == expected)    
